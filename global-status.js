3741603aa4144a0baf8026afada998af074e5dd6
dveltanescu998-hub <dveltanescu998@gmail.com>
2026-03-15 01:41:40 +0100

    Update global-status.js

--- global-status.js
+++ global-status.js
@@ -1,1 +1,18 @@
+// Logica pentru procente control mondial
+let worldControl = {
+    zenith: 45, // % din suprafață
+    khaos: 40,  // % din suprafață
+    neutral: 15
+};
 
+function updateWorldDominance(faction, areaCaptured) {
+    // Calculăm impactul cuceririi asupra hărții mondiale
+    if(faction === 'ZENITH') {
+        worldControl.zenith += areaCaptured;
+        worldControl.khaos -= areaCaptured;
+    } else {
+        worldControl.khaos += areaCaptured;
+        worldControl.zenith -= areaCaptured;
+    }
+    renderLeaderboard();
+}
